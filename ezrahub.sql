e34090d20873c0f644774d62360a82319a978753
Will Najar <will.najar@gmail.com>
2013-07-28 14:41:46 -0400

    fixed SQL dump

--- ezrahub.sql
+++ ezrahub.sql
@@ -1,30 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 3.4.11.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jul 28, 2013 at 01:53 AM
--- Server version: 5.5.29
--- PHP Version: 5.4.6-1ubuntu1.2
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `ezrahub`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bans`
---
 
 CREATE TABLE IF NOT EXISTS `bans` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -37,10 +18,6 @@ CREATE TABLE IF NOT EXISTS `bans` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
---
--- Table structure for table `laravel_migrations`
---
-
 CREATE TABLE IF NOT EXISTS `laravel_migrations` (
   `bundle` varchar(50) NOT NULL,
   `name` varchar(200) NOT NULL,
@@ -48,9 +25,8 @@ CREATE TABLE IF NOT EXISTS `laravel_migrations` (
   PRIMARY KEY (`bundle`,`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Table structure for table `messages`
---
+INSERT INTO `laravel_migrations` (`bundle`, `name`, `batch`) VALUES
+('authority', '2012_01_24_205457_add_authority_tables', 1);
 
 CREATE TABLE IF NOT EXISTS `messages` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -63,10 +39,6 @@ CREATE TABLE IF NOT EXISTS `messages` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=32 ;
 
---
--- Table structure for table `posts`
---
-
 CREATE TABLE IF NOT EXISTS `posts` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `thread_id` int(11) unsigned NOT NULL,
@@ -77,11 +49,58 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `updated_at` datetime NOT NULL,
   `edited_by` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35559 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35577 ;
 
---
--- Table structure for table `threadviews`
---
+CREATE TABLE IF NOT EXISTS `reps` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_from` int(10) unsigned NOT NULL,
+  `user_to` int(10) unsigned NOT NULL,
+  `rep_amount` int(11) NOT NULL,
+  `sign` tinyint(4) NOT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=127 ;
+
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+INSERT INTO `roles` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'admin', '2013-03-21 03:19:35', '2013-03-21 03:19:35'),
+(2, 'mod', '2013-03-21 03:19:35', '2013-03-21 03:19:35'),
+(3, 'beta', '2013-03-29 21:31:28', '2013-03-29 21:31:28');
+
+CREATE TABLE IF NOT EXISTS `role_user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+INSERT INTO `role_user` (`id`, `user_id`, `role_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '2013-03-21 03:19:35', '2013-03-21 03:19:35');
+
+CREATE TABLE IF NOT EXISTS `threads` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `poster_ip` int(10) unsigned NOT NULL,
+  `sticky` tinyint(1) DEFAULT '0',
+  `lock` tinyint(1) DEFAULT '0',
+  `bumplock` tinyint(1) DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2134 ;
 
 CREATE TABLE IF NOT EXISTS `threadviews` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -90,14 +109,25 @@ CREATE TABLE IF NOT EXISTS `threadviews` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33200 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33224 ;
 
---
--- Dumping data for table `users`
---
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `bio` text NOT NULL,
+  `reputation` int(20) NOT NULL,
+  `last_login_ip` int(10) unsigned NOT NULL,
+  `last_seen` datetime NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=110 ;
 
 INSERT INTO `users` (`id`, `email`, `password`, `name`, `created_at`, `updated_at`, `bio`, `reputation`, `last_login_ip`, `last_seen`, `active`) VALUES
-(0, 'anonymous@coward.com', 'anonymous', 'anonymous', '2013-03-23 00:00:00', '2013-06-08 07:50:18', 'I am not one user; I am a cowardly group users that didn''t make an account or didn''t want an internet pseudonym attached to my post. Beware of what I say, and take it with a grain of salt.  ', -5000, 0, '2013-03-23 00:00:00', 0);
+(0, 'anonymous@coward.com', 'anonymous', 'anonymous', '2013-03-23 00:00:00', '2013-06-08 07:50:18', 'I am not one user; I am a cowardly group users that didn''t make an account or didn''t want an internet pseudonym attached to my post. Beware of what I say, and take it with a grain of salt.  ', 0, 0, '2013-03-23 00:00:00', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
